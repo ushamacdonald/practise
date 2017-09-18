7f63ea8c1414b0226da79abd727a98ebd1d887f2
Usha <usha.macdonald@gmail.com>
2017-09-18 16:09:43 +1200

    rat component rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
-import Greetings from './Greetings'
+import Rats from './Rats'
 
 const App = () => (
   <Router>
     <div className='app-container'>
       <h1>Hello World</h1>
-      <Route exact path="/" component={Greetings} />
+      <Route exact path="/" component={Rats} />
     </div>
   </Router>
 )
---
+++ client/components/Rats.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Rats = () => {
+  <div>
+    <h1>Meet the Rats</h1>
+  </div>
+}
+
+
+export default Rats
